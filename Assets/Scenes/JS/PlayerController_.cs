b6b4996ded0e6a8d88a6d4627bb4cdb19acc260c
LAPTOP-K6HCTSO2\jisoo <boy7442@gmail.com>
2022-10-03 13:45:00 +0900

    Temp

---
+++ Assets/Scenes/JS/PlayerController_.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class PlayerController_ : MonoBehaviour
+{
+    Vector3 moveDirection;
+    float moveSpeed = 4f;
+
+    private void Update()
+    {
+        bool hasControl = (moveDirection != Vector3.zero);
+        if (hasControl)
+        {
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+    }
+    public void OnMove(InputValue value)
+    {
+        Vector2 input = value.Get<Vector2>();
+        moveDirection = new Vector3(input.x, input.y, 0f);
+        Debug.Log(moveDirection);
+    }
+
+}
