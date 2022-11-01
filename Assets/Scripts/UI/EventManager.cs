5bea30ac5be478d066a47d5c1489cb02df2c96d7
Hollyblue <da.young.yun13@gmail.com>
2022-11-01 20:30:40 +0900

    1. 2022/11/01 - 윤여정 상점&메인화면 및 자잘한 UI 업로드

---
+++ Assets/Scripts/UI/EventManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventManager : MonoBehaviour
+{
+    /*public GameObject Ui;
+    public Button Por;
+
+   /* private void Start()
+    {
+        Ui.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Ui.activeSelf == false) //인스펙터창 보이는지 체크
+            Por.onClick.AddListener(show);
+
+        else
+            Por.onClick.AddListener(exit);
+
+    }
+
+    void show()
+    {
+        Ui.SetActive(true);
+       // Invoke("Hide", 3); //3초 뒤 사라짐. 
+    }
+
+    void exit()
+    {
+        Ui.SetActive(false);
+    }
+ void Hide() { Ui.SetActive(false); }
+
+
+    */
+}
