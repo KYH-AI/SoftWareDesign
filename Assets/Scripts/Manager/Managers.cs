689b9f2a4fbb68f66d5ba84de05029d56aaab1c9
LAPTOP-K6HCTSO2\jisoo <boy7442@gmail.com>
2022-11-22 18:20:32 +0900

    Managers.cs 수정

--- Assets/Scripts/Manager/Managers.cs
+++ Assets/Scripts/Manager/Managers.cs
@@ -19,6 +19,12 @@ public class Managers : MonoBehaviour
     private SkillEffectVolumeManager skillEffectVolume;
     public static SkillEffectVolumeManager SkillEffectVolume { get { return Instance.skillEffectVolume; } }
 
+    private UIManager ui;
+    public static UIManager UI { get { return Instance.ui; } }
+
+    private Player player;
+    public static Player Player { get { return Instance.player; } }
+
     private void Awake()
     {
         if(instance == null)
