d378d8af098eedbc9aed906d5e463004d8a2ab21
Hollyblue <da.young.yun13@gmail.com>
2022-11-29 16:47:27 +0900

    병합 충돌 해결

--- Assets/Scripts/BossSpawnEffect.cs
+++ Assets/Scripts/BossSpawnEffect.cs
@@ -11,13 +11,8 @@ public class BossSpawnEffect : MonoBehaviour
     public PlayableDirector playableDirector;
     public TimelineAsset timeline;
     public Text bossEffectText;
-<<<<<<< HEAD
-   // Image bossEffectImage;
-    //public Sprite[] bossImageList;
-=======
     public Image bossEffectImage;
     public Sprite[] bossImageList;
->>>>>>> main
     public CinemachineVirtualCamera bossCamera;
 
 
@@ -70,25 +65,6 @@ public class BossSpawnEffect : MonoBehaviour
         {
             case Define.Stage.STAGE1:
                 bossEffectText.text = "첫 번째 보스";
-<<<<<<< HEAD
-                //bossEffectImage.sprite = bossImageList[0];
-                break;
-            case Define.Stage.STAGE2:
-                bossEffectText.text = "두 번째 보스";
-                //bossEffectImage.sprite = bossImageList[1];
-                break;
-            case Define.Stage.STAGE3:
-                bossEffectText.text = "세 번째 보스";
-                //bossEffectImage.sprite = bossImageList[2];
-                break;
-            case Define.Stage.STAGE4:
-                bossEffectText.text = "네 번째 보스";
-               // bossEffectImage.sprite = bossImageList[3];
-                break;
-            case Define.Stage.Boss:
-                bossEffectText.text = "최종 보스";
-                //bossEffectImage.sprite = bossImageList[4];
-=======
                 bossEffectImage.sprite = bossImageList[0];
                 break;
             case Define.Stage.STAGE2:
@@ -106,7 +82,6 @@ public class BossSpawnEffect : MonoBehaviour
             case Define.Stage.Boss:
                 bossEffectText.text = "최종 보스";
                 bossEffectImage.sprite = bossImageList[4];
->>>>>>> main
                 break;
 
         }
